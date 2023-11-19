da3de0e45bc6206f18c974bfd8b0ef2c1bfe93e8
Kking03 <kkorolev03@gmial.com>
2023-11-20 02:25:29 +0300

    Matrix-table

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -6,9 +6,17 @@ app.config.from_object('config')
 @app.route('/')
 def hello_world():
     return render_template("mainpage.html", title = 'Графы')
+
 @app.route("/index")
 def index():
-	return render_template("index.html", title = 'Графы')
+    return render_template("index.html", title = 'Графы')
+
+@app.route('/process', methods=['POST'])
+def process_data():
+    size = request.get_data(as_text=True) # получение данных
+    print('Размер матрицы:', size) # вывод полученных данных
+    return 'Данные успешно получены на сервере' # требуется вовзрат текстового значения
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,51 +1,107 @@
+// формирование таблицы
 function get_matrix()
 {
-    var size = document.getElementById('range_size_of_matrix').value
+    var size = document.getElementById('range_size_of_matrix').value // размер матрицы
 
-    var div_matr = document.getElementById('matrix_input')
-    div_matr.innerHTML = div_matr.innerHTML + '<div><button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Green</button></div>'
-    var table = document.createElement('table')
-    // header line
-    table.innerHTML = ''
-    var line = '<tr class="bg-red-900">'
-    for (var i = 0; i <= size; i += 1)
-    {
-        if (i == 0)
-        {
-            line += '<td class="bg-red-900">1</td>'
-        }
-        else
-        {
-            line += '<td class="bg-grey-500">2</td>'
-        }
+    // отправка данных для обработки на Python
+    fetch('/process', {
+        method: 'POST', // тип запроса
+        body: size // отправляем данные на сервер
+    })
+
+    document.getElementById('matrix_size_div').classList.add('hidden') // прячем кнопку ввода размера матрицы
+
+    var matrixContainer = document.getElementById('matrix_input'); // блок для вставки матрицы
+    
+    //matrixContainer.innerHTML = ''; // очищаем блок перед созданием новой матрицы
+
+    // создаем таблицу для матрицы смежности
+    var table = document.createElement('table');
+    table.id = 'matrix-table'
+
+    // заголовок таблицы
+    var thead = document.createElement('thead');
+    var headerRow = document.createElement('tr');
+    var emptyHeaderCell = document.createElement('th');
+    headerRow.appendChild(emptyHeaderCell); //пустая ячейка в верхнем левом углу
+
+    // Заголовки столбцов (вершины)
+    for (var i = 1; i <= size; i++) {
+        var vertexHeaderCell = document.createElement('td');
+        vertexHeaderCell.textContent = 'x' + i;
+        headerRow.appendChild(vertexHeaderCell);
     }
-    line += '</tr>'
-    // table
-    for (var i = 1; i <= size; i += 1)
-    {
-  
-        line += '<tr>'
-        for (var j = 0; j <= size; j += 1)
-        {
-            if (j == 0)
-            {
-                line += '<td class="bg-grey-500">2</td>'
-            }
-            else if (i == j)
-            {
-                line += '<td class="bg-red-900">1</td>'
-            }
-            else
-            {
-                line += '<td class="bg-blue-500">3</td>'
-            }
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Тело таблицы
+    var tbody = document.createElement('tbody');
+    for (var i = 1; i <= size; i++) {
+        var row = document.createElement('tr');
+        var vertexCell = document.createElement('td');
+        vertexCell.textContent = 'x' + i; // Подписи вершин (строки)
+        row.appendChild(vertexCell);
+
+        // Ячейки матрицы для ввода данных (input)
+        for (var j = 1; j <= size; j++) {
+            var cell = document.createElement('td');
+            var input = document.createElement('input');
+            input.type = 'text';
+            input.name = 'matrixCell' + i + '_' + j;
+            cell.appendChild(input);
+            row.appendChild(cell);
         }
-        line += '</tr>'
+        tbody.appendChild(row);
     }
-    table.innerHTML = '<tbody>' + line + '</tbody>'
-    div_matr.innerHTML = '<div>' + `<table id="matrix_input_table_${size} class="w-full border-separate text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"` + table.innerHTML + '</table></div>' + div_matr.innerHTML
-    document.getElementById('matrix_size_div').classList.add('hidden')
+    table.appendChild(tbody);
+    matrixContainer.appendChild(table);
+
+    // добавление кнопки ввода
+    matrixContainer.innerHTML = matrixContainer.innerHTML + '<div><button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Enter</button></div>'
 }
+
+    
+
+    // var table = document.createElement('table')
+    // // header line
+    // table.innerHTML = ''
+    // var line = '<tr class="bg-red-900">'
+    // for (var i = 0; i <= size; i += 1)
+    // {
+    //     if (i == 0)
+    //     {
+    //         line += '<td class="bg-red-900">1</td>'
+    //     }
+    //     else
+    //     {
+    //         line += '<td class="bg-grey-500">2</td>'
+    //     }
+    // }
+    // line += '</tr>'
+    // // table
+    // for (var i = 1; i <= size; i += 1)
+    // {
+  
+    //     line += '<tr>'
+    //     for (var j = 0; j <= size; j += 1)
+    //     {
+    //         if (j == 0)
+    //         {
+    //             line += '<td class="bg-grey-500">2</td>'
+    //         }
+    //         else if (i == j)
+    //         {
+    //             line += '<td class="bg-red-900">1</td>'
+    //         }
+    //         else
+    //         {
+    //             line += '<td class="bg-blue-500">3</td>'
+    //         }
+    //     }
+    //     line += '</tr>'
+    // }
+    // table.innerHTML = '<tbody>' + line + '</tbody>'
+    // div_matr.innerHTML = '<div>' + `<table id="matrix_input_table_${size} class="w-full border-separate text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"` + table.innerHTML + '</table></div>' + div_matr.innerHTML
 
 // Change the icons inside the button based on previous settings
 
