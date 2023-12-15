f6bf6689439f21561bdbcb18fac64c77e810ba80
VelandMerl <91304258+VelandMerl@users.noreply.github.com>
2023-12-15 20:44:45 +0300

    theme save between pages

--- static/js/main.js
+++ static/js/main.js
@@ -308,11 +308,11 @@ function change_Size(element)
 function set_theme()
 {
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        console.log('light')
+        document.documentElement.classList.add('dark');
         document.getElementById('theme-toggle-light-icon').classList.remove('hidden');
     } else {
+        document.documentElement.classList.remove('dark');
         document.getElementById('theme-toggle-dark-icon').classList.remove('hidden');
-        console.log('dark')
     }
 }
 
@@ -329,9 +329,11 @@ function change_theme()
         if (localStorage.getItem('color-theme') === 'light') {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
+            localStorage.theme = 'dark';
         } else {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
+            localStorage.theme = 'light';
         }
 
     // if NOT set via local storage previously
@@ -339,9 +341,11 @@ function change_theme()
         if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
+            localStorage.theme = 'light';
         } else {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
+            localStorage.theme = 'dark';
         }
     }
 }
