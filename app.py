44a9a4f5aa285a59bf3c87addabe7453e9e59871
VelandMerl <91304258+VelandMerl@users.noreply.github.com>
2023-12-15 22:54:05 +0300

    Made not bad navbar

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import Algorithms.Minimal_spanning_tree as st # импорт модуля Кол
 
 from flask_sqlalchemy import SQLAlchemy ## имплементация бд
 
+key_alg_name_dict = {"ml": ["/strong_connectivity/malgrange", "Алгоритм Мальгранжа"], "ks": ["/strong_connectivity/kosaraju", "Алгоритм Косарайю"], "dm": ["/topological_sort/demukron", "Алгоритм Демукрона"], "dfs":["/topological_sort/depth_first_search", "Поиск в глубину"], "pr": ["/minimal_spanning_tree/prim", "Алгоритм Прима"], "kr": ["/minimal_spanning_tree/kraskal", "Алгоритм Краскала"]}
+
+
 app = Flask(__name__)
 app.config.from_object('config')
 app.secret_key = 'your_secret_key' # секретный ключ для подписы данных сессии
@@ -46,7 +49,7 @@ class Example (db.Model):
 # начальная страница
 @app.route('/')
 def home():
-    return render_template("index.html", title = 'Графы')
+    return render_template("index.html", show_alg_btn = [], title = 'Графы')
 
 # универсальная функция для сохранения матрицы в сессии
 @app.route('/set_data_to_session', methods=['POST'])
@@ -83,30 +86,30 @@ def set_dbdata():
 # алгоритмы Андрей
 @app.route("/strong_connectivity")
 def strong_connectivity_input():
-    return render_template("strong_connectivity.html", title = 'Разбиение графа', ml_desc = Algorithm.query.filter_by(key = 'ml').first(), ks_desc = Algorithm.query.filter_by(key = 'ks').first())
+    return render_template("strong_connectivity.html", show_alg_btn = [], title = 'Разбиение графа', ml_desc = Algorithm.query.filter_by(key = 'ml').first(), ks_desc = Algorithm.query.filter_by(key = 'ks').first())
 
 @app.route('/strong_connectivity/malgrange')
 def Malgrange():
     # matrix = [ [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [1, 1, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0] ]
     alg_input, steps, alg_result = sc.algorithm_Malgrange(session['matrix'])
-    return render_template("main.html", title = 'Мальгранж', alg_input = alg_input, steps = steps, alg_result = alg_result)
+    return render_template("main.html", show_alg_btn = ["ml", "ks"], title = 'Мальгранж', alg_title = "Алгоритм Мальгранжа", alg_input = alg_input, steps = steps, alg_result = alg_result)
 
 @app.route('/strong_connectivity/kosaraju')
 def Kosaraju():
     # matrix = [ [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [1, 1, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0] ]
     alg_input, steps, alg_result = sc.algorithm_Kosaraju(session['matrix'])
-    return render_template("main.html", title = 'Косарайю', alg_input = alg_input, steps = steps, alg_result = alg_result)
+    return render_template("main.html", show_alg_btn = ["ml", "ks"], title = 'Косарайю', alg_title = "Алгоритм Косарайю", alg_input = alg_input, steps = steps, alg_result = alg_result)
 
 # алгоритмы Кирилла
 @app.route("/topological_sort")
 def topological_sort_input():
-    return render_template("topological_sort.html", title = 'Топологическая сортировка', dm_desc = Algorithm.query.filter_by(key = 'dm').first(), dfs_desc = Algorithm.query.filter_by(key = 'dfs').first())
+    return render_template("topological_sort.html", show_alg_btn = [], title = 'Топологическая сортировка', dm_desc = Algorithm.query.filter_by(key = 'dm').first(), dfs_desc = Algorithm.query.filter_by(key = 'dfs').first())
 
 @app.route('/topological_sort/depth_first_search')
 def depth_first_search():
     # matrix = [ [0, 1, 0, 1, 1], [0, 0, 0, 0, 1], [0, 0, 0, 0, 0], [0, 1, 0, 0, 1], [0, 0, 1, 0, 0] ]
     alg_input, steps, alg_result = ts.algorithm_depth_first_search(session['matrix'])
-    return render_template("main.html", title = 'Поиск в глубину', alg_input = alg_input, steps = steps, alg_result = alg_result)
+    return render_template("main.html", show_alg_btn = ["dm", "dfs"], title = 'Поиск в глубину', alg_title = "Поиск в глубину", alg_input = alg_input, steps = steps, alg_result = alg_result)
 
 @app.route('/topological_sort/demukron')
 def demukron():
@@ -114,22 +117,22 @@ def demukron():
 
     alg_input, steps, alg_result = ts.demukron(session['matrix'])
     # alg_input, steps, alg_result = ts.demukron(matrix)
-    return render_template("main.html", title = 'Поиск в глубину', alg_input = alg_input, steps = steps, alg_result = alg_result)
+    return render_template("main.html", show_alg_btn = ["dm", "dfs"], title = 'Демукрон', alg_title = "Алгоритм Демукрона", alg_input = alg_input, steps = steps, alg_result = alg_result)
 
 # алгоритмы Коли
 @app.route("/minimal_spanning_tree")
 def minimal_spanning_tree():
-    return render_template("minimal_spanning_tree.html", title = 'Минимальный остов', kr_desc = Algorithm.query.filter_by(key = 'kr').first(), pr_desc = Algorithm.query.filter_by(key = 'pr').first())
+    return render_template("minimal_spanning_tree.html", show_alg_btn = [], title = 'Минимальный остов', kr_desc = Algorithm.query.filter_by(key = 'kr').first(), pr_desc = Algorithm.query.filter_by(key = 'pr').first())
 
 @app.route('/minimal_spanning_tree/kraskal')
 def kraskal():
     alg_input, steps, alg_result = st.kraskal(session['matrix'])
-    return render_template("main.html", title = 'Краскал', alg_input = alg_input, steps = steps, alg_result = alg_result)
+    return render_template("main.html", show_alg_btn = ["kr", "pr"], title = 'Краскал', alg_title = "Алгоритм Краскала", alg_input = alg_input, steps = steps, alg_result = alg_result)
 
 @app.route('/minimal_spanning_tree/prim')
 def prim():
     alg_input, steps, alg_result = st.prim(session['matrix'])
-    return render_template("main.html", title = 'Прим', alg_input = alg_input, steps = steps, alg_result = alg_result)
+    return render_template("main.html", show_alg_btn = ["kr", "pr"], title = 'Прим', alg_title = "Алгоритм Прима", alg_input = alg_input, steps = steps, alg_result = alg_result)
 
 
 if __name__ == '__main__':
