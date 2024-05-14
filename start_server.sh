6574395a1da6ff4db5bbb6469eba3e86fe0fc624
VelandMerl <91304258+VelandMerl@users.noreply.github.com>
2024-05-14 18:38:35 +0300

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,5 @@
 sleep 30 # Need time for postgesql server to create database and start to accept TCP connections
+echo "Filling DB with values..."
 python /usr/src/app/fill_db.py
+echo "Starting server..."
 python /usr/src/app/app.py
